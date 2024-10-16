e3a8eca70b70242b3978e13c6a9fb5979a7445f5
Marc <158296221+Axo34080@users.noreply.github.com>
2024-10-16 13:09:36 +0200

    Optimisation, ajout descriptions

--- projets.js
+++ projets.js
@@ -1,38 +1,33 @@
 const projects = document.querySelectorAll('.project');
 let currentProject = 0;
 
-function showNextProject() {
+function showProject(index) {
+    // Afficher le projet courant et le rendre actif
     projects[currentProject].classList.remove('active');
-    setTimeout(() => {
-        projects[currentProject].style.display = 'none';
+    projects[currentProject].style.display = 'none'; // Cacher le projet courant
 
-        currentProject = (currentProject + 1) % projects.length;
+    currentProject = index; // Mettre à jour l'index du projet courant
+    projects[currentProject].style.display = 'block'; // Afficher le nouveau projet
 
-        projects[currentProject].style.display = 'block';
-        setTimeout(() => {
-            projects[currentProject].classList.add('active');
-        }, 50);
-    }, 500);
+    // Utiliser requestAnimationFrame pour ajouter la classe active
+    requestAnimationFrame(() => {
+        projects[currentProject].classList.add('active');
+    });
+}
+
+function showNextProject() {
+    const nextProject = (currentProject + 1) % projects.length; // Calculer le prochain projet
+    showProject(nextProject);
 }
 
 function showPrevProject() {
-    projects[currentProject].classList.remove('active');
-    setTimeout(() => {
-        projects[currentProject].style.display = 'none';
-
-        // Calculer le projet précédent
-        currentProject = (currentProject - 1 + projects.length) % projects.length;
-
-        projects[currentProject].style.display = 'block';
-        setTimeout(() => {
-            projects[currentProject].classList.add('active');
-        }, 50);
-    }, 500);
+    const prevProject = (currentProject - 1 + projects.length) % projects.length; // Calculer le projet précédent
+    showProject(prevProject);
 }
 
 // Flèches de navigation
 document.querySelector('.next').addEventListener('click', showNextProject);
 document.querySelector('.prev').addEventListener('click', showPrevProject);
 
-// Lancer le défilement automatique toutes les 2 secondes
+// Lancer le défilement automatique toutes les 10 secondes
 setInterval(showNextProject, 10000);
