f5849909d3dabadedd0e83852ff9d4b6289a7b12
Marc <158296221+Axo34080@users.noreply.github.com>
2024-10-17 16:08:17 +0200

    changement de design pour la section projet

--- projets.js
+++ projets.js
@@ -1,33 +1,7 @@
 const projects = document.querySelectorAll('.project');
-let currentProject = 0;
 
-function showProject(index) {
-    // Afficher le projet courant et le rendre actif
-    projects[currentProject].classList.remove('active');
-    projects[currentProject].style.display = 'none'; // Cacher le projet courant
-
-    currentProject = index; // Mettre à jour l'index du projet courant
-    projects[currentProject].style.display = 'block'; // Afficher le nouveau projet
-
-    // Utiliser requestAnimationFrame pour ajouter la classe active
-    requestAnimationFrame(() => {
-        projects[currentProject].classList.add('active');
-    });
-}
-
-function showNextProject() {
-    const nextProject = (currentProject + 1) % projects.length; // Calculer le prochain projet
-    showProject(nextProject);
-}
-
-function showPrevProject() {
-    const prevProject = (currentProject - 1 + projects.length) % projects.length; // Calculer le projet précédent
-    showProject(prevProject);
-}
-
-// Flèches de navigation
-document.querySelector('.next').addEventListener('click', showNextProject);
-document.querySelector('.prev').addEventListener('click', showPrevProject);
-
-// Lancer le défilement automatique toutes les 10 secondes
-setInterval(showNextProject, 10000);
+projects.forEach((project) => {
+  project.addEventListener('click', () => {
+    const projectLink = project.querySelector('a').getAttribute('href');
+    window.open(projectLink, '_blank');
+  });
